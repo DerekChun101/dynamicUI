448d85ff2889a11cbfe08f2377291859529c9f2f
Derek Chun <98774886+DerekChun101@users.noreply.github.com>
2024-01-25 18:36:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# templateRepo
+
+Eslint: https://eslint.org/docs/latest/use/getting-started
+
+Vs Code tutorial: https://www.digitalocean.com/community/tutorials/linting-and-formatting-with-eslint-in-vs-code
+
+
+Prettier: https://prettier.io/
+
+Eslint-cofig-prettier: https://github.com/prettier/eslint-config-prettier#installation
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "To-Do",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
